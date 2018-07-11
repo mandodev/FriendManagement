c569830ad87567e2468439dedb185ad84d961548
Armand Caesar <armand.caesar@gmail.com>
2018-07-11 13:53:07 +0700

    fix test

--- module/notification/controller.go
+++ module/notification/controller.go
@@ -1,5 +1,11 @@
 package notification
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 //Controller : Notification Controller
 type Controller struct {
 	notificationService *Service
@@ -8,4 +14,10 @@ type Controller struct {
 //NewController : function to instantiate new controller
 func NewController(service *Service) (*Controller, error) {
 	return &Controller{notificationService: service}, nil
+}
+
+//Subscribe : function that allow user to subscribe update based on email address
+func (c *Controller) Subscribe(ctx *gin.Context) {
+
+	ctx.JSON(http.StatusOK, gin.H{"success": true})
 }
--- test/notification/controller_test.go
+++ test/notification/controller_test.go
@@ -1,1 +1,28 @@
 package notification
+
+import (
+	"bytes"
+	"net/http"
+	"testing"
+
+	"github.com/FriendManagement/shared"
+	"github.com/FriendManagement/shared/config"
+	"github.com/FriendManagement/test"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSubscribeUpdate(t *testing.T) {
+	cfg, err := config.New("../../shared/config/")
+	assert.Empty(t, err)
+	configuration := *cfg
+
+	payload := bytes.NewBuffer([]byte(`{"requestor":"requestor@example.com", "target":"target@example.com"}`))
+	routerInstance := shared.NewRouter(configuration)
+	router := routerInstance.SetupRouter()
+
+	response := test.DispatchRequest(router, "POST", "/api/v1/notification/subscribe", payload)
+	s := string(response.Body.Bytes())
+	assert.NotEmpty(t, s)
+	assert.Equal(t, http.StatusOK, response.Code)
+	assert.Equal(t, "{\"success\":true}", s)
+}
