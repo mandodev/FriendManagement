9751c9686a99533283a7652c18af6908775ea85d
Armand Caesar <armand.caesar@gmail.com>
2018-07-11 19:19:25 +0700

    Finish controller, service and test

--- test/notification/service_test.go
+++ test/notification/service_test.go
@@ -1,1 +1,24 @@
 package notification
+
+import (
+	"testing"
+
+	"github.com/FriendManagement/module/notification"
+	messages "github.com/FriendManagement/module/notification/messages"
+	"github.com/FriendManagement/shared/config"
+	"github.com/FriendManagement/shared/data"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSubscribe(t *testing.T) {
+	cfg, _ := config.New("../../shared/config/")
+	dbInstance, _ := data.NewDbFactory(cfg)
+	conn, _ := dbInstance.DBConnection()
+
+	service, err := notification.NewService(conn)
+	request := &messages.SubscribeRequest{Requestor: "friend1@example.com", Target: "common@example.com"}
+	result, err := service.Subscribe(request)
+
+	assert.NotNil(t, result)
+	assert.Nil(t, err)
+}
